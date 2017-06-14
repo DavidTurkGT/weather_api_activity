718fa359ad35927940a19985568a4aced8204eed
Robby Dore <rdore88@gmail.com>
2017-06-14 10:20:58 -0400

    Used weather underground api

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Acc", "application/json");
+// var myHeaders = new Headers();
+// myHeaders.append("Content-Type", "application/json");
+// myHeaders.append("Acc", "application/json");
+//
+// var myInit = {
+//   method: 'GET',
+//   headers: myHeaders,
+//   mode: 'no-cors',
+//   cache: 'default'
+// };
 
-var myInit = {
-  method: 'GET',
-  headers: myHeaders,
-  mode: 'no-cors',
-  cache: 'default'
-};
+let data;
+let current_temp = document.getElementById("temp")
 
-let data = [];
-
-fetch("https://www.metaweather.com/api/location/2357024/2017/06/14/", myInit)
+fetch("http://api.wunderground.com/api/a0fad745509bd909/geolookup/conditions/q/GA/Atlanta.json")
   .then(function(res, rej) {
-    console.log(res, rej)
-    return res.json();
+    console.log("response:", res);
+     return res.json();
+    //  let temp = data.location.feelslike_string
+    //  console.log("current_temp:", temp);
   })
   .then(function(json) {
     console.log(json);
+    let temp = json.current_observation.feelslike_string
+    console.log("current_temp:", temp);
   });
